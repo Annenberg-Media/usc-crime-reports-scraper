96f9b492c74f8196e0b2d3f4ace4077d02d8bb2d
Ritom Gupta <ritomgupta99@gmail.com>
2024-04-09 13:58:47 -0700

    use mongo from .env

--- src/test_api.py
+++ src/test_api.py
@@ -1,23 +1,27 @@
 import requests
 import json
 from parse import read_and_parse
+from decouple import config
 
 url = f"https://data.mongodb-api.com/app/data-wpkwm/endpoint/data/v1/action/insertMany"
 print("PARSING CSV TO JSON")
-json_data = read_and_parse('./031924.pdf')
+json_data = read_and_parse("./031924.pdf'")
 print("JSON DATA: ")
 print(json_data)
+
+MONGO_KEY = config("MONGODB_KEY")
+# print(MONGO_KEY)
 headers = {
-  "apiKey": "XaNzeoJPPapRYXyBX5dgVLVTQYBegrrSEDWdIf7YAV5KEVN8uN6vTWw5ZQ2jj9sM",
-  "Content-Type": "application/json",
-  "Accept": "application/json"
+    "apiKey": MONGO_KEY,
+    "Content-Type": "application/json",
+    "Accept": "application/json",
 }
 
 payload = {
     "dataSource": "USC-AnnMedia-WebTeam",
     "database": "dps",
     "collection": "dps-test",
-    "documents": json_data
+    "documents": json_data,
 }
 
 print("Sending request:")
